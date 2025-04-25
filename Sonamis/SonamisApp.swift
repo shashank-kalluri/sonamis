c6088ee8928831d3fe530be6de8b3e07f8700f0d
Shashank Kalluri <skalluri123@hotmail.com>
2025-04-25 12:45:48 -0400

    Initial Commit

---
+++ Sonamis/SonamisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonamisApp.swift
+//  Sonamis
+//
+//  Created by Shashank Kalluri on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SonamisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
